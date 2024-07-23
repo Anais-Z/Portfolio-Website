a60e8a4db27847333478be6c06e6cf6a977f9dea
Anais <anaiszed447@gmail.com>
2024-07-23 14:24:20 -0400

    Created the initial background

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HiArrowNarrowRight } from 'react-icons/hi'
+
+const Home = () => {
+  return (
+    <div className='w-full h-screen bg-teal-400'>
+      <p>fff</p>
+    </div>
+  )
+}
+
+export default Home
