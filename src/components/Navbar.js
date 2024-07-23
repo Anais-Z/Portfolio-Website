1e8892b04eac2b3360a2035fd3b4d7b08d46d8ac
Anais <anaiszed447@gmail.com>
2024-07-23 12:33:34 -0400

    Created initial background with text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import {FaBars, FaTimes} from 'react-icons/fa'
 const Navbar = () => {
   return (
-    <div>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-slate-900 text-white'>
+        <div>
+            <p style={{width: '90px'}}>Anais</p>
+        </div>
+
+        {/* main menu */}
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Experience</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+
+        {/* hamburger */}
+        <div> </div>
+
+         {/* mobile menu */}
+         <div> </div>
+
+          {/* social media icons */}
+          <div> </div>
       
     </div>
   )
