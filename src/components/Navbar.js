7ba7056b10e43ec3ece23c3f234addf6f59512a9
Anais <anaiszed447@gmail.com>
2024-07-23 13:03:57 -0400

    Developed initial navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
+
 const Navbar = () => {
+
+    const [nav,setNav] = useState(false)
+
+    const handleClick = () => setNav(!nav)
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-slate-900 text-white'>
         <div>
@@ -8,24 +14,34 @@ const Navbar = () => {
         </div>
 
         {/* main menu */}
-        <div>
-            <ul>
+        
+            <ul className='hidden md:flex'>
                 <li>Home</li>
                 <li>About</li>
                 <li>Experience</li>
                 <li>Projects</li>
                 <li>Contact</li>
             </ul>
-        </div>
+        
 
         {/* hamburger */}
-        <div> </div>
+        <div onClick={handleClick} className='md:hidden z-10'>
+            {!nav ? <FaBars/> : <FaTimes/>}
+         </div>
 
          {/* mobile menu */}
-         <div> </div>
+         
+         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-slate-800 flex flex-col justify-center items-center'}>
+                <li className='py-6 text-4xl'>Home</li>
+                <li className='py-6 text-4xl' >About</li>
+                <li className='py-6 text-4xl'>Experience</li>
+                <li className='py-6 text-4xl'>Projects</li>
+                <li className='py-6 text-4xl'>Contact</li>
+            </ul>
+           
 
           {/* social media icons */}
-          <div> </div>
+          <div className='hidden'> </div>
       
     </div>
   )
