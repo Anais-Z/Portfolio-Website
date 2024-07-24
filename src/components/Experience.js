cc3d283b660925187995ee5ff40f0f0c40589b55
Anais <anaiszed447@gmail.com>
2024-07-24 13:20:22 -0400

    Created the experience page and the initial content.

---
+++ src/components/Experience.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+
+const Experience = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-white text-black'>
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col h-full space-y-16'>
+
+
+        <div>
+            <h2> Experience </h2>
+
+        </div>
+        <div>
+            <p>Full Stack Developer - Sheridan College (S-Sense Platform)</p>
+            <p>January 2023 - August 2023</p>
+
+            <ul class="list-disc ml-5">
+                <li> Point 1</li>
+                <li> Point 2</li>
+                <li> Point 3</li>
+                <li> Point 4</li>
+            </ul>
+        </div>
+
+        <div>
+            <p>Web Developer - Oakville Film Festival</p>
+            <p>May 2022 - August 2022</p>
+            <ul class="list-disc ml-5">
+                <li> Point 1</li>
+                <li> Point 2</li>
+                <li> Point 3</li>
+                <li> Point 4</li>
+            </ul>
+        </div>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Experience
