6487a86d5f5725c0a39a2eb701328c7f3cb3a566
Anais <anaiszed447@gmail.com>
2024-07-24 14:03:30 -0400

    Added the experience component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Experience from "./components/Experience";
 
 function App() {
   return (
     <div>
        <Navbar />
        <Home />
+       <Experience />
     </div>
   );
 }
