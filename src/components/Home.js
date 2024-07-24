5d2f3a7ed5a7ffb836c782bf2c02d3142ec8dc91
Anais <anaiszed447@gmail.com>
2024-07-23 23:53:11 -0400

    Created the text and styled it

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { HiArrowNarrowRight } from 'react-icons/hi'
+
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-teal-400'>
-      <p>fff</p>
+    <div name="name" className='w-full h-screen bg-white'>
+      
+      {/** Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-red-500'>Hello, I am</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-black'>Anais</h1>
+        <h2 className='text-blue-700 py-4 max-w-[700px]'>I'm a full stack developer who specialize in building web and mobile apps with an interest 
+          AI/ml learning
+        </h2>
+        
+      </div>
+
+    
     </div>
   )
 }
