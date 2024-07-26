74ba0b924a5ee341c2a297df8e846559ec5a3b65
Anais <anaiszed447@gmail.com>
2024-07-26 16:24:30 -0400

    Added the tech stack for the jobs

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -16,19 +16,34 @@ const Experience = () => {
                 Full Stack Developer - Sheridan College (S-Sense Platform)</p>
             <p>January 2023 - August 2023</p>
 
+            <div className='flex flex-row gap-2 text-white mt-2'>
+                <p className='border-4 border-black bg-black rounded-md text-sm text-center px-2'>C#</p>
+                <p className='border-4 border-black bg-black rounded-md text-sm text-center px-2'>ASP.NET</p>
+                <p className='border-4 border-black bg-black rounded-md text-sm text-center px-2'>Xamarin</p>
+                <p className='border-4 border-black bg-black rounded-md text-sm text-center px-2'>MongoDB</p>
+            </div>
+
             
             <ul class="list-disc ml-5 mt-6 space-y-4">
-                <li> Developed a REST API using the .NET framework with MongoDB to optimize data management to enhance system efficiency </li>               
-                <li> Reported and assisted with the development of new and existing features</li>
-                <li> Followed internal processes and procedures for versioning and documentation of code</li>
+                <li> Developed and deployed a robust Web API using the .NET framework, seamlessly integrated with MongoDB to optimize data management and enhance system efficiency </li>               
+                <li> Refactored and debugged critical issues within app functionalities, ensuring proper user experience and app features working as designed</li>
+                <li> Implemented dynamic font sizing, voice control, and haptic feedback functionalities, enhancing the app's accessibility </li>
                 <li> Participated in testing and implementing feedback based on bug fixes and feature updates</li>
             </ul>
+
+            
             
         </div>
 
         <div>
             <p class="text-lg font-bold text-black">Web Developer - Oakville Film Festival</p>
             <p>May 2022 - August 2022</p>
+
+            <div className='flex flex-row gap-2 text-white mt-2'>
+                <p className='border-4 border-black bg-black rounded-md text-sm text-center px-2'>HTML</p>
+                <p className='border-4 border-black bg-black rounded-md text-sm text-center px-2'>CSS</p>
+                <p className='border-4 border-black bg-black rounded-md text-sm text-center px-2'>JavaScript</p>
+            </div>
 
             <ul class="list-disc ml-5 mt-6 space-y-4">
                 <li> Collaborated with a team to create a web application prototype using FIGMA, ensuring alignment with project objectives and user needs</li>
